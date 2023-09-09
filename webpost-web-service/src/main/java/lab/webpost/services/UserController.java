eacfdfcc85ce008fc7b85d59490e98a6cab7ca20
guymn <gjp2267@gmail.com>
2023-09-09 14:15:18 +0700

    User C

--- webpost-web-service/src/main/java/lab/webpost/services/UserController.java
+++ webpost-web-service/src/main/java/lab/webpost/services/UserController.java
@@ -1,23 +1,32 @@
 package lab.webpost.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lab.webpost.domain.User;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
     @Autowired
-    UserRepository userRepository;
+    private UserRepository userRepository;
 
 
     //TODO: end point for validate user by username 
-    public ResponseEntity<User> getUserByUsername(String username) {
-       return null;
+    @GetMapping("/{username}")
+    public ResponseEntity<User> getUserByUsername(@PathVariable String username) {
+        Optional<User> optUser = userRepository.findByUsername(username);
+        if(!optUser.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+       return ResponseEntity.ok(optUser.get());
     }
 }
